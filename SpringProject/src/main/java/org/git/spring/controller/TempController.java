68a0778b42cb370ae0a0eed355f94c378dd9f591
STripathy <STripathy>
2016-08-19 16:40:13 -0700

    First Spring Project Commit

---
+++ SpringProject/src/main/java/org/git/spring/controller/TempController.java
@@ -0,0 +1,23 @@
+package org.git.spring.controller;
+
+import javax.inject.Inject;
+import org.git.spring.service.TempService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/temp")
+public class TempController {
+
+	private TempService tempService;
+	
+	@Inject
+	public TempController(TempService ts){
+		this.tempService = ts;
+	}
+	@RequestMapping("/static")
+	public @ResponseBody String getStaticTempText(){
+		return "This is a static temp text/"+tempService.getStaticTempTest();
+	}
+}
---
+++ SpringProject/src/main/java/org/git/spring/service/TempService.java
@@ -0,0 +1,8 @@
+package org.git.spring.service;
+
+public class TempService {
+	
+	public String getStaticTempTest(){
+		return "Static Test Text from Temp Service";
+	}
+}
