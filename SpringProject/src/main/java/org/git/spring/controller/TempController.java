8bebb414d8690190511294161e573e4848a6b09b
Sujit Tripathy <sujit.tripathy@icloud.com>
2016-08-19 22:31:29 -0700

    text changed to updated

--- SpringProject/src/main/java/org/git/spring/controller/TempController.java
+++ SpringProject/src/main/java/org/git/spring/controller/TempController.java
@@ -18,6 +18,6 @@ public class TempController {
 	}
 	@RequestMapping("/static")
 	public @ResponseBody String getStaticTempText(){
-		return "This is a static temp text/"+tempService.getStaticTempTest();
+		return "This is a static temp text [updated]/"+tempService.getStaticTempTest();
 	}
 }
