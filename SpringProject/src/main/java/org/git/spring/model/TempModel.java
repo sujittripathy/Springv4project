8d591126fab4a304df8b8dfedd589500d1a8baec
sujittripathy <sujittripathy@192.168.0.3>
2016-08-20 09:26:40 -0700

    added a new model class

---
+++ SpringProject/src/main/java/org/git/spring/model/TempModel.java
@@ -0,0 +1,20 @@
+package org.git.spring.model;
+
+public class TempModel {
+
+		private String city;
+		private int temp;
+		
+		public String getCity() {
+			return city;
+		}
+		public void setCity(String city) {
+			this.city = city;
+		}
+		public int getTemp() {
+			return temp;
+		}
+		public void setTemp(int temp) {
+			this.temp = temp;
+		}
+}
