2e7ff6a0aaabd4cc0f451830b19871d124a56b41
sujittripathy <sujittripathy@192.168.0.3>
2016-08-20 09:27:36 -0700

    added a new REST method

--- SpringProject/src/main/java/org/git/spring/controller/TempController.java
+++ SpringProject/src/main/java/org/git/spring/controller/TempController.java
@@ -1,9 +1,13 @@
 package org.git.spring.controller;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+
+import org.git.spring.model.TempModel;
 import org.git.spring.service.TempService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -19,5 +23,15 @@ public class TempController {
 	@RequestMapping("/static")
 	public @ResponseBody String getStaticTempText(){
 		return "This is a static temp text [updated on MAC 2 now]/"+tempService.getStaticTempTest();
+	}
+	
+	@RequestMapping(value="/citytemp", method=RequestMethod.GET)
+	public @ResponseBody TempModel getTemp(HttpServletRequest request){
+		//String name = request.getParameter("name");
+		TempModel t=new TempModel();
+		t.setCity("Brea");
+		t.setTemp(90);
+		//return "Hello "+name;
+		return t;
 	}
 }
